4b63d77e86e885186618f61ba177a02e3f6b6c40
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-03 17:10:35 +0200

    Completed Image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Vue: Hello World!"
+            message: "Vue: Hello World!",
+            url: "../img/logo.jpg"
         }
     }
 }).mount("#app");
